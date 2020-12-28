a70eb818df989555a496951e3b7ced1d13458595
Thomas Laetsch <laetsch@Thomass-Mac-mini.local>
2020-12-28 15:48:20 -0500

    Updated discussion in descriptive ipynb; started module emopy.py

---
+++ emopy.py
@@ -0,0 +1,12 @@
+import requests as rq
+from lxml import html
+import re
+
+
+class emopy(object):
+    """**class emopy**
+    For emoji extraction from text using the unicode emoji list found at
+        https://unicode.org/emoji/charts/full-emoji-list.html
+    """
+    def __init__(self):
+        pass
