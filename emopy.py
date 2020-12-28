74e3441f373494d95931c5dfc62d047b15aeb67e
Thomas Laetsch <laetsch@Thomass-Mac-mini.local>
2020-12-28 16:31:38 -0500

    Added negative emoji search to ipynb; updated emopy.py with the first few basic methods

--- emopy.py
+++ emopy.py
@@ -9,4 +9,25 @@ class emopy(object):
         https://unicode.org/emoji/charts/full-emoji-list.html
     """
     def __init__(self):
+        self.emojis_re = None
+        self.not_emojis_re = None
         pass
+    
+    @staticmethod
+    def _create_positive_regex(emojis):
+        emojis_re_str = "(" + "|".join(["(%s)"%el.replace("*","\*") for el in emojis]) + ")"
+        return re.compile(emojis_re_str)
+    
+    @staticmethod
+    def _create_negative_regex(emojis):
+        not_emojis_re_str = "[^" + "".join([el.replace("*","\*") for el in emojis]) + "]"
+        return re.compile(not_emojis_re_str)
+    
+    def text_emojis_from_doc(self, doc):
+        extr = self.emojis_re.findall(doc)
+        emojis, descrs = zip(*[(tup[0], self.descrs[tup[1:].index(tup[0])]) for tup in extr])
+        text = "".join(self.not_emojis_re.findall(doc))
+        return emojis, descrs, text
+    
+    def text_emojis_from_docs(self, docs):
+        return [self.text_emojis_from_doc(doc) for doc in docs]
